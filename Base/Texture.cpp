01fdd5a41dc50de231e4a29b14f2e1f98720f5e8
jonathanhr <jonathan.hortelano@gmail.com>
2015-11-18 21:27:42 +0100

    asdf

---
+++ Base/Texture.cpp
@@ -0,0 +1,14 @@
+#include "Named.h"
+
+CNamed::CNamed(const CXMLTreeNode &TreeNode) : m_Name(TreeNode.GetPszProperty("name", "")) {
+}
+		
+CNamed::CNamed(const std::string &Name): m_Name(Name){}
+	
+void CNamed::SetName(const std::string &Name){
+	m_Name = Name;
+}
+
+const std::string &CNamed::GetName(){
+	return m_Name;
+}
---
+++ Base/Texture.h
@@ -0,0 +1,19 @@
+/*#include "Named.h"
+#include "TwDirect3D11.h"
+
+class CTexture : CNamed
+{
+	private:
+		ID3D11ShaderResourceView *m_Texture;
+		ID3D11SamplerState *m_SamplerState;
+		virtual bool LoadFile();
+		void Unload();
+
+	public:
+		CTexture();
+		virtual ~CTexture();
+		bool Load(const std::string &Filename);
+		void Activate(unsigned int StageId);
+		bool Reload();
+};
+*/
